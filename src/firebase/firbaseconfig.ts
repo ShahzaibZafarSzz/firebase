f186abee66b9d06837e2ed5216263817f6fa41d9
ShahzaibZafarSzz <168672583+ShahzaibZafarSzz@users.noreply.github.com>
2024-09-08 17:22:52 +0500

    Add files via upload

---
+++ src/firebase/firbaseconfig.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAUkhmgTYK9eYIF5RRvyTWFPfaMvfAurRE",
+    authDomain: "batch-11smit.firebaseapp.com",
+    projectId: "batch-11smit",
+    storageBucket: "batch-11smit.appspot.com",
+    messagingSenderId: "975730649186",
+    appId: "1:975730649186:web:56ba9b48050acc4d53893b",
+    measurementId: "G-SF7Y3ZGXWJ"
+  };
+
+  export const app = initializeApp(firebaseConfig);
